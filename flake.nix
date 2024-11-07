b2785ea75b1ce79c1f9d8e014dcd5cdc06ff9b95
niumination <niumination@gmail.com>
2024-11-08 01:20:43 +0700

    update

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
